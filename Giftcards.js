fb4aee660de88406b88978e506ad05359ce45b90
ExScaliba <kadejohn255@yahoo.com>
2024-12-09 23:26:04 -0600

    More css payment changes

--- Giftcards.js
+++ Giftcards.js
@@ -1,3 +1,4 @@
+//Modal JavaScript for multiple modals
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
